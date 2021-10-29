cdb64de26db26b5df2e12c3e9124a4386b594bff
Lewis Relph <lewis.relph@ibm.com>
2021-10-29 14:53:00 +0100

    Tests for environment var parameter overwrite username/password

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const Generator = require('@asyncapi/generator');
-const { existsSync } = require('fs');
+const { existsSync, readFileSync } = require('fs');
 const crypto = require('crypto');
 
 const MAIN_TEST_RESULT_PATH = path.join('test', 'temp', 'integrationTestResult');
@@ -118,5 +118,209 @@ describe('template integration tests using the generator', () => {
     for (const index in expectedFiles) {
       expect(existsSync(path.join(OUTPUT_DIR, expectedFiles[index]))).toBe(true);
     }
+  });
+
+  it('default env password is passw0rd', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE
+    };
+  
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+    
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let password = exportedEnv.MQ_ENDPOINTS[0].APP_PASSWORD;
+
+    // Check default password
+    expect(password).toBe('passw0rd')
+  });
+
+  it('default password overwritten on param provide', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE,
+      password: 'paramProvidedPassword'
+    };
+  
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+    
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let password = exportedEnv.MQ_ENDPOINTS[0].APP_PASSWORD;
+
+    // Check default password
+    expect(password).toBe('paramProvidedPassword')
+  });
+
+  it('default password overwritten by system env', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE
+    };
+  
+    // Set app_password env variable
+    process.env.APP_PASSWORD = "envAppPassword"
+
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+
+    // Clear env, not sure if this will persist
+    process.env.APP_PASSWORD = null
+
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let password = exportedEnv.MQ_ENDPOINTS[0].APP_PASSWORD;
+
+    // Check default password
+    expect(password).toBe('envAppPassword')
+  });
+
+  it('password parameter overwrites environment variable', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE,
+      password: 'paramProvidedPassword'
+    };
+  
+    // Set app_password env variable
+    process.env.APP_PASSWORD = "envAppPassword"
+
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+
+    // Clear env, not sure if this will persist
+    process.env.APP_PASSWORD = null
+
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let password = exportedEnv.MQ_ENDPOINTS[0].APP_PASSWORD;
+
+    // Check default password
+    expect(password).toBe('paramProvidedPassword')
+  });
+
+  it('default env username is app', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE
+    };
+  
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+    
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let username = exportedEnv.MQ_ENDPOINTS[0].APP_USER;
+
+    // Check default password
+    expect(username).toBe('app')
+  });
+
+  it('default username overwritten on param provide', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE,
+      user: 'paramUsername'
+    };
+  
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+    
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let username = exportedEnv.MQ_ENDPOINTS[0].APP_USER;
+
+    // Check default password
+    expect(username).toBe('paramUsername')
+  });
+
+  it('default username overwritten by system env', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE
+    };
+  
+    // Set app_user env variable
+    process.env.APP_USER = "envAppUsername"
+
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+
+    // Clear env, not sure if this will persist
+    process.env.APP_USER = null
+
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let username = exportedEnv.MQ_ENDPOINTS[0].APP_USER;
+
+    // Check default password
+    expect(username).toBe('envAppUsername')
+  });
+
+  it('username parameter overwrites environment variable', async () => {
+    jest.setTimeout(30000);
+  
+    const OUTPUT_DIR = generateFolderName();
+    const PACKAGE = 'com.ibm.mq.different.jms';
+    const PACKAGE_PATH = path.join(...PACKAGE.split('.'));
+    const params = {
+      server: 'production',
+      package: PACKAGE,
+      user: 'paramProvidedUsername'
+    };
+  
+    // Set app_user env variable
+    process.env.APP_USER = "envAppUsername"
+
+    const generator = new Generator(path.normalize('./'), OUTPUT_DIR, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test', 'mocks/many-messages.yml'));
+
+    // Clear env, not sure if this will persist
+    process.env.APP_USER = null
+
+    // Read in JSON file
+    let exportedEnv = JSON.parse(readFileSync(path.join(OUTPUT_DIR, 'env.json')));
+    let username = exportedEnv.MQ_ENDPOINTS[0].APP_USER;
+
+    // Check default password
+    expect(username).toBe('paramProvidedUsername')
   });
 });
